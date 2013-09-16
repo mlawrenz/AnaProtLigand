25c8a4b33bb0a6d7f47ec002355563c143948e18
Morgan <morganlawrenz@gmail.com>
2013-09-16 16:19:09 -0700

    coarse check

--- coarse_ass.py
+++ coarse_ass.py
@@ -20,10 +20,10 @@ def main(coarse_val, orig_val, rcut):
         for type in types:
             if 'ass' in type:
                 ass=io.loadh('%s/Data/Assignments.h5' % dirs[key])
-                data[key][type]=ass
+                data[key][type]=ass['arr_0']
             elif 'dist' in type:
                 ass=io.loadh('%s/Data/Assignments.h5.distances' % dirs[key])
-                data[key][type]=ass
+                data[key][type]=ass['arr_0']
             elif 'rmsd' in type:
                 rmsd=numpy.loadtxt('%s/Gens.rmsd.dat' % dirs[key])
                 data[key][type]=rmsd
@@ -32,109 +32,55 @@ def main(coarse_val, orig_val, rcut):
                 data[key][type]=gens
     unboundmap=dict()
     boundmap=dict()
-    unboundstates=dict()
-    unboundrmsd=dict()
+    #unboundstates=dict()
+    #unboundrmsd=dict()
     # build map dict for orig to coarse unbound states, bound will stay same
-    for j in range(0, data['orig']['ass']['arr_0'].shape[0]):
-        for (n,i) in enumerate(data['orig']['ass']['arr_0'][j]):
-            if i!=-1:
+    newass=-1*numpy.ones(( data['orig']['ass'].shape[0], data['orig']['ass'].shape[1]), dtype=int)
+    for j in range(0, data['orig']['ass'].shape[0]):
+        for (n,i) in enumerate(data['orig']['ass'][j]):
+            # if unbound
+            if i != -1:
                 if data['orig']['rmsd'][i] > float(rcut):
-                    state=data['coarse']['ass']['arr_0'][j][i]
-                    if state!=-1:
-                        if i not in unboundstates.keys():
-                            unboundstates[i]=[]
-                            unboundrmsd[i]=[]
-                        if state not in unboundstates[i]:
-                            unboundstates[i].append(state)
-                            unboundrmsd[i].append(data['coarse']['dist']['arr_0'][j][i])
+                    state=data['coarse']['ass'][j][n]
+                    newass[j][n]=state+10000
                 else:
-                    if i not in boundmap.keys():
-                        boundmap[i]=i
-    # unbound map needs unbound states as keys with bound states in lists
+                    newass[j][n]=i
+    count=0
+    unique=set(newass.flatten())
+    boundmap=dict()
     unboundmap=dict()
-    for unboundstate in unboundstates.values():
-        if len(unboundstate) > 1:
-            boundstate=[i for i in unboundstates.keys() if unboundstates[i]==unboundstate ]
-            boundstate=boundstate[0]
-            rmsd=numpy.array(unboundrmsd[boundstate])
-            frame=numpy.where(rmsd==min(rmsd))[0]
-            best=unboundstate[frame]
-            if best not in unboundmap.keys():
-                unboundmap[best]=[]
-            if boundstate not in unboundmap[best]:
-                unboundmap[best].append(boundstate)
+    for x in unique:
+        locations=numpy.where(newass==x)
+        newass[locations]=count
+        if x >= 10000:
+            unboundmap[count]=(x-1000)
         else:
-            unboundstate=unboundstate[0]
-            if unboundstate not in unboundmap.keys():
-                unboundmap[unboundstate]=[]
-            if boundstate not in unboundmap[unboundstate]:
-                unboundmap[unboundstate].append(boundstate)
-    # deal with micro states that are sent to different coarse states
-    map=dict()
-    bound=0
-    for i in sorted(boundmap.keys()):
-        if i not in map.keys():
-            map[i]=bound
-            bound+=1
-    unbound=max(map.values())+1
-    unbound_start=max(map.values())+1
-    unboundtrack=dict()
-    import pdb
-    pdb.set_trace()
-    for state in sorted(unboundmap.keys()):
-        for i in unboundmap[state]:
-            map[i]=unbound
-            if i not in unboundtrack.keys():
-                unboundtrack[i]=state
-            elif i in unboundtrack.keys() and unboundtrack[i]!=state:
-                print "state mismatch"
-                import pdb
-                pdb.set_trace()
-        unbound+=1
-    newgens=proj.empty_traj()
+            boundmap[count]=x
+        count+=1
+    io.saveh('%s/Coarsed_r%s_Assignments.h5' % (dirs['orig'], rcut), newass)
     subdir='%s/Coarsed_r%s_PDBs/' % (dirs['orig'], rcut)
     if not os.path.exists(subdir):
         os.mkdir(subdir)
     ohandle=open('%s/Coarsed%s_r%s_Gens.rmsd.dat' % (subdir, coarse_val, rcut), 'w')
     b=data['orig']['gens']['XYZList'].shape[1]
     c=data['orig']['gens']['XYZList'].shape[2]
-    ## add to newgens accoring to KEY values and their mapped states, will have multiple states per macrostate dict VALUE
-    ## loop over values not keys, maybe write out PDBs of states?
-    print "unbound start %s" % unbound_start
-    for macro in sorted(set(map.values())):
-        print "writing coarse gen %s out of %s pdbs" % (macro, len(set(map.values())))
-        if macro >= unbound_start:
-            import pdb
-            pdb.set_trace()
-            gens=proj.empty_traj()
-            a=len(sorted(map.keys()))
-            newrmsd=numpy.zeros((a))
-            states=[i for i in map.keys() if map[i]==macro]
-            structure=proj.empty_traj()
-            structure['XYZList']=numpy.zeros((len(states), b, c), dtype=numpy.float32)
-            for (n, state) in enumerate(states):
-                unboundstate=unboundtrack[state]
-                structure['XYZList'][n]=data['coarse']['gens']['XYZList'][unboundstate]
-                structure.save_to_pdb('%s/Coarsed%s_r%s_Gen%s-%s.pdb' % (subdir, coarse_val, rcut, macro, n))
-                ohandle.write('%s\t%s\t%s\n' % (macro, n, data['orig']['rmsd'][unboundstate]))
-        else:
-            states=[i for i in map.keys() if map[i]==macro]
-            if len(states) > 1 :
-                print "wrong"
-                import pdb
-                pdb.set_trace()
-            state=states[0]
-            structure=proj.empty_traj()
-            structure['XYZList']=numpy.zeros((1, b, c), dtype=numpy.float32)
-            structure['XYZList'][0]=data['orig']['gens']['XYZList'][state]
-            structure.save_to_pdb('%s/Coarsed%s_r%s_Gen%s.pdb' % (subdir, coarse_val, rcut, macro))
-            ohandle.write('%s\t%s\n' % (macro, data['orig']['rmsd'][state]))
-    newass=numpy.zeros(( data['orig']['ass']['arr_0'].shape[0], data['orig']['ass']['arr_0'].shape[1]), dtype=int)
-    for j in range(0, data['orig']['ass']['arr_0'].shape[0]):
-        for (n,i) in enumerate(data['orig']['ass']['arr_0'][j]):
-            if i!=-1:
-                newass[j][n]=map[i]
-    io.saveh('%s/Coarsed%s_r%s_Assignments.h5' % (subdir, rcut, coarse_val), newass)
+    dicts=[boundmap, unboundmap]
+    names=['bound', 'unbound']
+    labels=['orig', 'coarse']
+    total=len(boundmap.keys()) + len(unboundmap.keys())
+    structure=proj.empty_traj()
+    structure['XYZList']=numpy.zeros((total, b, c), dtype=numpy.float32)
+    count=0
+    import pdb
+    pdb.set_trace()
+    for (name, label, mapdata) in zip( names, labels, dicts):
+        print "writing coarse gen %s out of %s pdbs" % (count, len(mapdata.keys()))
+        for i in sorted(mapdata.keys()):
+            macro=mapdata[i]
+            structure['XYZList'][count]=data[label]['gens']['XYZList'][macro]
+            ohandle.write('%s\t%s\t%s\n' % (name, count, data[label]['rmsd'][macro]))
+            count+=1
+    structure.save_to_xtc('%s/Coarsed%s_r%s_Gens.xtc' % (subdir, coarse_val, rcut))
     return data
 
 def parse_commandline():
