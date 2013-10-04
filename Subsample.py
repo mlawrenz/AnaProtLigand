ebc5f2b711d22c7cd4796c458fc969b5e37eb271
Morgan Lawrenz <mlawrenz@stanford.edu>
2013-10-04 16:27:45 -0700

    added subsample script

---
+++ Subsample.py
@@ -0,0 +1,42 @@
+from msmbuilder import io
+import os
+import optparse
+from msmbuilder import Project
+import numpy
+
+def main(file):
+    ass=io.loadh(file)
+    dir=os.path.dirname(file)
+    base=os.path.basename(file)
+    newdir='%s/subsample' % dir
+    if not os.path.exists(newdir):
+        os.mkdir(newdir)
+    p=Project.load_from('%s/ProjectInfo.yaml' % dir.split('Data')[0])
+    data=dict()
+    iterations=int(ass['arr_0'].shape[1]/10.0)
+    start=max(p.traj_lengths)
+    for iter in range(0, iterations):
+        new=start-10
+        if new < 10:
+            break
+        data[new]=-numpy.ones((ass['arr_0'].shape[0], new), dtype=int)
+        for i in range(0, ass['arr_0'].shape[0]):
+            data[new][i]=ass['arr_0'][i][:new]
+        start=new
+
+    for key in sorted(data.keys()):
+        print data[key].shape
+        io.saveh('%s/%s_sub%s.h5' % (newdir, base.split('.h5')[0], key), data[key])
+
+def parse_commandline():
+    parser = optparse.OptionParser()
+    parser.add_option('-f', '--file', dest='file',
+                      help='assignments file')
+    (options, args) = parser.parse_args()
+    return (options, args)
+
+#run the function main if namespace is main
+if __name__ == "__main__":
+    (options, args) = parse_commandline()
+    main(file=options.file)
+
